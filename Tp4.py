7be9ca6541c4c4d38325a538bb0baaf996cb3f54
Agostina Acosta <agosacosta38@gmail.com>
2026-06-10 16:28:03 -0300

    contra

--- Tp4.py
+++ Tp4.py
@@ -1,1 +1,7 @@
-print("Hola Mundo")
+usuario = input("Ingrese su usuario: ")
+clave = input("Ingrese su contraseña: ")
+
+if len(clave) < 8:
+    print("Contraseña insegura")
+else:
+    print("Acceso permitido")
